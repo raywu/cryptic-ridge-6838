e48748a2ebae9fac90ffda0f696b97825990ca1b
Ray Wu <raymond.cj.wu@gmail.com>
2013-05-23 19:32:08 +0800

    Add database repair code in ./wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -91,3 +91,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_ALLOW_REPAIR', true);
+/* Reset Database Plugin
